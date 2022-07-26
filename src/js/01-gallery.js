47f7b93bd7c9c5b59df1caabadc9873f4aa36037
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-26 17:15:08 +0300

    Решает задачу 1 Модуля 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = createGalleryLi(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+
+// FUNCTION ==================================
+
+function createGalleryLi(array) {
+    return array
+    .map(item => {
+        return `
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`
+    })
+    .join('');
+}
