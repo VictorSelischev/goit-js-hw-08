e88479614c029a104b1cb3618043f1b7af84b00a
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-28 23:10:27 +0300

    Убирает ошибку начального значения проигрывателя

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,5 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-// var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -32,4 +31,19 @@ player.on(
   }, 1000)
 );
 
-player.setCurrentTime(load(LOCALSTORAGE_KEY_VIDEO));
+player
+  .setCurrentTime(load(LOCALSTORAGE_KEY_VIDEO))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
