c3e33ca6e8b5c2a40632ecf61b31527c516cd664
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-28 15:21:54 +0300

    Использует локальное хранилище

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// console.log(player);
+
+let playerLocalStorage = {};
+
+player.on('play', function () {
+    console.log(timeupdate);
+    console.log('played the video!');
+
+        const currentTime = timeupdate.seconds;
+    console.log(currentTime);
+
+    localStorage.setItem("videoplayer-current-time", "What the fack");
+    const vcT = localStorage.getItem("videoplayer-current-time");
+
+    console.log(vcT);
+    // localStorage.removeItem("videoplayer-current-time");
+});
+
+
+
+
+
+
+
+
+
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
